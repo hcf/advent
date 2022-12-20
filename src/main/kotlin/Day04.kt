5ecb8697f22219a1a9c9f9500fa9b1cc758f0a02
Hans-Christian Fjeldberg-Gustavson <hans-christian@diffia.com>
2022-12-20 21:39:48 +0100

    feat: Adding kotlin skeleton for day 4
    Adding input files and quick suggestion to setup

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,4 @@
+object Day04 {
+    fun solvePart1(lines: List<String>): Int = Int.MAX_VALUE // Your solution goes here
+    fun solvePart2(lines: List<String>): Int = Int.MAX_VALUE // Your solution goes here
+}
